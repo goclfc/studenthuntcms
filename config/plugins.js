ca8386c19d4f264fee3cce0389f9a73593ca8c73
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2023-02-26 11:18:54 +0400

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,36 +1,25 @@
-
-  module.exports = ({ env }) => ({
-    email: {
-      config: {
-        provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-          defaultFrom: 'gocha.berulava@gmail.com',
-          defaultReplyTo: 'juliasedefdjian@strapi.io',
-          testAddress: 'gocha.berulava@gmail.com',
-        },
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "gocha.berulava@gmail.com",
+        defaultReplyTo: "juliasedefdjian@strapi.io",
+        testAddress: "gocha.berulava@gmail.com",
       },
     },
-  });
-
-  module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
     },
-    // ...
-  });
-  
+  },
+});
